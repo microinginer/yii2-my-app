575c0f10749eef0a351f2955a10807fa2dd97af7
inginer <ruslanmadatov@yandex.ru>
2017-02-10 14:42:26 +0300

    config dotenv

--- web/index.php
+++ web/index.php
@@ -1,12 +1,33 @@
 <?php
 
 // comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
 
-require(__DIR__ . '/../vendor/autoload.php');
-require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../env.php';
+require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
-$config = require(__DIR__ . '/../config/web.php');
+$config = require __DIR__ . '/../config/web.php';
+
+
+// debug tools
+function d()
+{
+    foreach (func_get_args() as $val) {
+        \yii\helpers\VarDumper::dump($val, 10, true);
+    }
+}
+
+function dx()
+{
+    foreach (func_get_args() as $val) {
+        d($val);
+    }
+    exit;
+}
+
+function br()
+{
+    echo '<br><br>';
+}
 
 (new yii\web\Application($config))->run();
