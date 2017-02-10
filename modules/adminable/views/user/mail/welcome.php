082452cddec5f6d5898d7272d6f1dc42eec5ecd6
inginer <ruslanmadatov@yandex.ru>
2017-02-10 17:26:32 +0300

    fix mail template

--- modules/adminable/views/user/mail/welcome.php
+++ modules/adminable/views/user/mail/welcome.php
@@ -44,9 +44,5 @@ use yii\helpers\Html;
 <?php endif ?>
 
 <p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
-    <?= Html::a('Перейти в СРМ', \yii\helpers\Url::to(['/admin'], 'http')) ?>
-</p>
-
-<p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
     <?= Yii::t('user', 'If you did not make this request you can ignore this email') ?>.
 </p>
